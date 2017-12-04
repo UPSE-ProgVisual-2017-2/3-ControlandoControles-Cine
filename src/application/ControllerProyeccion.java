4225188a4c831a3944d9d726aaea7ae94abdab5e
Ivan A Sanchez <ivansanchez_1988@hotmail.com>
2017-12-04 14:57:42 -0500

    Commit inicial del Proyecto: Incluye modelo e interfaces para el ingreso de peliculas y de proyecciones. Pantallas estan atadas a controladores pero no estan enlazadas entre si (enlace entre pantallas). Aun no hay pase de mensajes entre pantallas (compartir objetos/Informacion). No hay rastreo anterior a los cambios del proyecto porque este semestre he estado muy ocupado.

---
+++ src/application/ControllerProyeccion.java
@@ -0,0 +1,100 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.omg.CORBA.portable.ValueFactory;
+
+import javafx.beans.property.StringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.SpinnerValueFactory;
+import javafx.scene.control.SpinnerValueFactory.IntegerSpinnerValueFactory;
+import model.Pelicula;
+import model.TipoPelicula;
+import model.TipoProyeccion;
+
+public class ControllerProyeccion {
+
+	@FXML ChoiceBox<Pelicula> chbPelicula;
+	@FXML DatePicker dateProyeccion;
+	@FXML Spinner<Integer> spnSala;
+	@FXML ChoiceBox<TipoProyeccion> chbTipoProyeccion;
+	@FXML Slider sldrHora;
+	@FXML Slider sldrMinuto;
+	@FXML Label lblHora;
+	@FXML Label lblMinuto;
+	
+	public ControllerProyeccion() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void initialize() 
+	{
+		//Creo el observable list con los valores del Enum TipoProyecciones
+		ObservableList<TipoProyeccion> observableTipoProyeccion = 
+				FXCollections.observableArrayList(TipoProyeccion.values());
+		//Amarro el observable list con el ChoiceBox
+		chbTipoProyeccion.setItems(observableTipoProyeccion);
+		
+		SpinnerValueFactory<Integer> spnValueFactoryInteger = 
+				new IntegerSpinnerValueFactory(1,10,1);
+		spnSala.setValueFactory(spnValueFactoryInteger);
+		
+		//Creo un metodo falso para cargar peliculas (hasta mientras).
+		cargarPeliculas();
+		
+		//Metodo para inicializar hora con valores max y min. 
+		//Luego amarro propiedad de texto de label a propiedad valor del Slider.
+		inicializarHora();
+		
+		inicializarMinutos();
+	}
+
+	private void inicializarHora() {
+		sldrHora.setMax(23);
+		sldrHora.setMin(0);
+		sldrHora.setValue(17);
+		
+		//Convertir el valor del slider value de double a entero
+		sldrHora.valueProperty().addListener(
+				(obs, oldVal, newVal) -> sldrHora.setValue(newVal.intValue()));
+				
+		lblHora.textProperty().bind(sldrHora.valueProperty().asString());		
+	}
+	
+	private void inicializarMinutos() {
+		sldrMinuto.setMax(59);
+		sldrMinuto.setMin(0);
+		sldrMinuto.setValue(0);
+		
+		sldrMinuto.valueProperty().addListener(
+				(obs, oldVal, newVal) -> sldrMinuto.setValue(newVal.intValue()));
+		
+		//Amarren la propiedad
+		lblMinuto.textProperty().bind(sldrMinuto.valueProperty().asString());
+	}
+
+
+	private void cargarPeliculas() {
+		List<Pelicula> peliculasCartelera = new ArrayList<Pelicula>();
+		
+		Pelicula p1 = new Pelicula("Thor: Ragnarok", TipoPelicula.COMIC);
+		Pelicula p2 = new Pelicula("El regreso de Rafico", TipoPelicula.DRAMA);
+		Pelicula p3 = new Pelicula("Examen de Programacion Visual 1", TipoPelicula.COMEDIA);
+		
+		peliculasCartelera.add(p1);
+		peliculasCartelera.add(p2);
+		peliculasCartelera.add(p3);
+		
+		ObservableList<Pelicula> observableListPelicula = FXCollections.observableArrayList(peliculasCartelera);
+		chbPelicula.setItems(observableListPelicula);
+	}
+
+}
